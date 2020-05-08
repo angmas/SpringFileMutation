5c4e9ef3393d0366ebe4a9e5f8d153b3789a827f
Angela Mashlan <angela.mashlan@gmail.com>
2020-05-08 06:10:41 -0700

    add AcordEventProcessorHelper constructor to do init

---
+++ src/main/java/com/angmas/mutation/service/PersAutoPolicyQuoteInqRqStartEventProcessorImpl.java
@@ -0,0 +1,11 @@
+package com.angmas.mutation.service;
+
+public class PersAutoPolicyQuoteInqRqStartEventProcessorImpl implements AcordElementStartEventProcessor {
+
+	@Override
+	public void doStartProcessing() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
