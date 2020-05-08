e9da604f1060f94447dc07f8cc6fc890a808de98
Angela Mashlan <angela.mashlan@gmail.com>
2020-05-07 20:36:41 -0700

    remove unused imports

--- src/main/java/com/angmas/mutation/controller/MutationController.java
+++ src/main/java/com/angmas/mutation/controller/MutationController.java
@@ -2,19 +2,12 @@ package com.angmas.mutation.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-
 import javax.xml.stream.XMLStreamException;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.angmas.mutation.domain.Greeting;
 import com.angmas.mutation.domain.Policy;
 import com.angmas.mutation.service.XmlToPolicyMappingService;
 
