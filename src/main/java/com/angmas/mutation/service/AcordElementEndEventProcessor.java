3e813e9f724dd5eba8db6aef69bde3353cb01b20
Angela Mashlan <angela.mashlan@gmail.com>
2020-05-09 06:21:18 -0700

    remove factory files

--- src/main/java/com/angmas/mutation/service/AcordElementEndEventProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.angmas.mutation.service;
-
-import javax.xml.stream.XMLStreamException;
-
-public interface AcordElementEndEventProcessor {
-
-	void doEndProcessing(AcordEventProcessorHelper helper) throws XMLStreamException;
-	
-}
--- src/main/java/com/angmas/mutation/service/AcordElementStartEventProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.angmas.mutation.service;
-
-import javax.xml.stream.XMLStreamException;
-
-public interface AcordElementStartEventProcessor {
-
-	void doStartProcessing(AcordEventProcessorHelper helper) throws XMLStreamException;
-	
-}
